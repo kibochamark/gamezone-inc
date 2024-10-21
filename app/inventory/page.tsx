f2d878ff82b04e98bf87cedd436c3653d47155de
kibochamark <mark.varu@student.cuk.ac.ke>
2024-10-21 07:09:57 +0300

    One time update

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -46,6 +46,22 @@ async function getLowStockSummary() {
 
     return inventory
 }
+
+// async function getLowStock() {
+//     let inventory: any
+//     try {
+//         inventory = await prisma.lowStockSummary.aggregate({
+//             _count: {
+//                 id: true,
+//             }
+//         }) ?? 0
+//     } catch (e: any) {
+//         console.log(e.message)
+//     }
+
+
+//     return inventory
+// }
 async function getRevenueSummary() {
     let inventory: any
     try {
@@ -72,6 +88,22 @@ const page = async () => {
     const revenue = await getRevenueSummary()
 
     const permissions = await getPermissions()
+
+    if(inventory.length > 0){
+        const products = inventory.filter((inv:any)=> inv.quantity < inv.threshold)
+        for( const item of products){
+            try{
+                    await prisma.lowStockSummary.create({
+                        data:{
+                            inventoryId:item.id,
+                            quantity:item.quantity,
+                        }
+                    })
+            }catch(e:any){
+                continue
+            }
+        }
+    }
 
     return (
         <div className='w-full  rounded-md h-full'>
