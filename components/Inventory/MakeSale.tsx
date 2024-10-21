13626cc4a30c0dd913c638b3a03d5238e15174a9
kibochamark <mark.varu@student.cuk.ac.ke>
2024-10-21 13:16:44 +0300

    Update on saletype

--- components/Inventory/MakeSale.tsx
+++ components/Inventory/MakeSale.tsx
@@ -131,7 +131,7 @@ const MakeSale = () => {
                 {formik.touched.saletype && formik.errors.saletype && (
                     <p className='text-sm text-red-600 tracking-tight leading-tight'>{formik.errors.saletype}</p>
                 )}
-                <select id="categoryId" name='categoryId' defaultValue={formik.values.saletype} onChange={formik.handleChange} onBlur={formik.handleBlur} disabled={formik.isSubmitting} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <select id="categoryId" name='saletype' defaultValue={formik.values.saletype} onChange={formik.handleChange} onBlur={formik.handleBlur} disabled={formik.isSubmitting} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option value="">select a sale type</option>
 
                     <option value={"DEBIT"}>Debit</option>
