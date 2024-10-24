ccdf9ba68b7eb6f3f4c16874808c580d45eb364b
kibochamark <mark.varu@student.cuk.ac.ke>
2024-10-24 07:52:39 +0300

    Update on sidebar

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -149,7 +149,7 @@ const SideBar = ({salescount}:{salescount:number}) => {
                                 Manage categories
                             </Link>
                             <Link
-                                href="#"
+                                href="/reports"
                                 className={`flex items-center gap-3 ${path.includes("reports") && "dark:bg-primary100 text-primary bg-primary50 "} rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary`}
                             >
                                 <LineChart className="h-4 w-4" />
