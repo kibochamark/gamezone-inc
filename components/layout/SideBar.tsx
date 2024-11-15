00563f57d79e5e3bc40fbd03e957dc37f46f0f79
kibochamark <mark.varu@student.cuk.ac.ke>
2024-11-15 10:52:57 +0300

    update on capital acc

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -159,7 +159,7 @@ const SideBar = ({salescount}:{salescount:number}) => {
                             </Link>
                             <Link
                                 href="/transactionreports"
-                                className={`flex items-center gap-3 ${path.includes("transaction") && "dark:bg-primary100 text-primary bg-primary50 "} rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary`}
+                                className={`flex items-center gap-3 ${path.startsWith("transaction") && "dark:bg-primary100 text-primary bg-primary50 "} rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary`}
                             >
                                 <LineChart className="h-4 w-4" />
                                 Account Reports
